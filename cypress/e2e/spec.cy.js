94ad8349063ac426094789fc26057826fc1ce3c8
NJI PERKINS <njipekins@gmail.com>
2026-08-21 03:37:01 +0100

    modified pipeline

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,106 +1,45 @@
-// <reference types="cypress" />
+/// <reference types="cypress" />
 describe('Web Application Tests', () => {
+  const userData = {
+    name: 'John Doe',
+    email: 'johndoe7031@example.com',
+    password: '123456789',
+    day: '10',
+    month: 'May',
+    year: '1990',
+    firstName: 'John',
+    lastName: 'Doe',
+    company: 'ABC Company',
+    address: '123 Main St',
+    address2: 'Apt 4B',
+    country: 'Canada',
+    state: 'Ontario',
+    city: 'Toronto',
+    zipcode: 'M5H 2N2',
+    mobile: '+1 416-123-4567'
+  };
+
+  const paymentData = {
+    nameOnCard: 'John Doe',
+    cardNumber: '4111111111111111',
+    cvc: '123',
+    expiryMonth: '12',
+    expiryYear: '2025'
+  };
+
   it('User can create an account and make a purchase', () => {
-    const timestamp = new Date().toISOString().replace(/[:.]/g, '-')
-    //register a new user account
-    cy.visit('https://automationexercise.com/ ');
+    cy.visit('https://automationexercise.com/');
     cy.title().should('eq', 'Automation Exercise');
-    cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
-    cy.get('[data-qa="signup-name"]').type('John Doe', { delay: 100 });
-    cy.get('[data-qa="signup-email"]').type('johndoe7030@example.com', { delay: 100 });
-    cy.get('[data-qa="signup-button"]').click();
-    cy.get('#id_gender1').check('Mr');
-    cy.get('[data-qa="password"]').type('123456789', { delay: 100 });
-    cy.get('[data-qa="days"]').select('10', { delay: 100 });
-    cy.get('[data-qa="months"]').select('May', { delay: 100 });
-    cy.get('[data-qa="years"]').select('1990', { delay: 100 });
-    cy.get('#newsletter').check();
-    cy.get('#optin').check();
-    cy.get('[data-qa="first_name"]').type('John', { delay: 100 });
-    cy.get('[data-qa="last_name"]').type('Doe', { delay: 100 });
-    cy.get('[data-qa="company"]').type('ABC Company', { delay: 100 });
-    cy.get('[data-qa="address"]').type('123 Main St', { delay: 100 });
-    cy.get('[data-qa="address2"]').type('Apt 4B', { delay: 100 });
-    cy.get('[data-qa="country"]').select('Canada', { delay: 100 });
-    cy.get('[data-qa="state"]').type('Ontario', { delay: 100 });
-    cy.get('[data-qa="city"]').type('Toronto', { delay: 100 });
-    cy.get('[data-qa="zipcode"]').type('M5H 2N2', { delay: 100 });
-    cy.get('[data-qa="mobile_number"]').type('+1 416-123-4567', { delay: 100 });
-    cy.get('[data-qa="create-account"]').click();
-    cy.get('.title > b').should('have.text', 'Account Created!');
-    cy.get('[data-qa="continue-button"]').click();
-    cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
-    //login with the newly created account
-    cy.get('[data-qa="login-email"]').type('johndoe7030@example.com', { delay: 100 });
-    cy.get('[data-qa="login-password"]').type('123456789', { delay: 100 });
-    cy.get('[data-qa="login-button"]').click();
-    cy.get('.shop-menu > .nav > :nth-child(10) > a').should('contain.text', 'Logged in as John Doe');
-    cy.scrollTo('bottom');
-    cy.wait(1000);
-    cy.scrollTo('top');
-    cy.contains('h2', 'Features Items').scrollIntoView();
-    // Add a product to the cart
-    cy.get('a[href="/product_details/1"]').click();
-    cy.scrollTo('bottom');
-    cy.wait(1000);
-    cy.scrollTo('top');
-    cy.get('#quantity').clear().type('5');
-    cy.contains('button', 'Add to cart').click();
-    cy.get('.modal-content').should('be.visible');
-    // Verify that the product has been added to the cart
-    cy.get('.modal-content').contains('Your product has been added to car').should('be.visible');
-    cy.get('.modal-content').contains('Continue Shopping').click();
-    cy.contains('button', 'Add to cart').click({ force: true });
-    cy.get('.modal-content a[href="/view_cart"]').click();
-    cy.get('.cart_info').contains('Blue Top').should('be.visible');
-    cy.get('.cart_info').contains('Rs. 500').should('be.visible');
-    cy.get('.cart_info').contains('5').should('be.visible');
-    cy.get('#cart_info')
-      .should('be.visible')
-      .then(($element) => {
-        const rawText = $element.text();
-        cy.log('The extracted cart text is: ' + rawText);
-        const priceMatch = rawText.match(/Rs\.\s?\d+/);
-        if (priceMatch) {
-          const extractedTotal = priceMatch[0];
-          cy.log('Extracted Total Price is: ' + extractedTotal);
-          expect(extractedTotal).to.contain('Rs.');
-        }
-      });
-    // Verify that the total price is displayed correctly
-    cy.get('.cart_info').contains('Total').should('be.visible');
-    cy.get('.btn.btn-default.check_out').click();
-    // Verify that the user is on the checkout page
-    cy.get('.heading').contains('Address Details').should('be.visible');
-    // Verify that the delivery and billing addresses are displayed correctly
-    cy.get('.page-subheading').contains('Your delivery address').should('be.visible');
-    cy.get('.page-subheading').contains('Your billing address').should('be.visible');
-    cy.get('a[href="/payment"]').click();
-    cy.get('.heading').should('be.visible');
-    // Verify that the user is on the payment page
-    cy.get('.heading').contains('Payment').should('be.visible');
-    cy.get('[data-qa="name-on-card"]').type('John Doe');
-    cy.get('[data-qa="card-number"]').type('4111111111111111');
-    cy.get('[data-qa="cvc"]').type('123');
-    cy.get('[data-qa="expiry-month"]').type('12');
-    cy.get('[data-qa="expiry-year"]').type('2025');
-    // Click the "Pay and Confirm Order" button
-    cy.get('[data-qa="pay-button"]').click();
-    // Verify that the order has been placed successfully
-    cy.get('[data-qa="order-placed"]').contains('Order Placed!').should('be.visible');
-    cy.contains('p', 'Congratulations! Your order has been confirmed!')
-      .should('be.visible');
-    // Verify that the user can log out successfully
-    cy.get('.btn.btn-default.check_out').should('be.visible');
-    cy.get('.btn.btn-primary').should('be.visible');
-    cy.get('.btn.btn-default.check_out').click();
-    cy.get('[data-qa="continue-button"]').click();
 
-    // Verify that the user can log out and log back in successfully
-    cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
-    cy.get('[data-qa="login-email"]').type('johndoe7030@example.com');
-    cy.get('[data-qa="login-password"]').type('123456789');
-    cy.get('[data-qa="login-button"]').click();
-  })
+    // Executing custom functions sequentially
+    cy.signUpAndCreateAccount(userData);
+    cy.login(userData.email, userData.password, userData.name);
+    cy.addFeaturedProductToCart('1', '5');
+    cy.verifyCartDetails('Blue Top', 'Rs. 500', '5');
+    cy.processCheckoutAndPayment(paymentData);
 
-})
+    // Final relogin assertion & verify cart status
+    cy.login(userData.email, userData.password);
+    cy.verifyCartIsEmpty();
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,117 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// 1. Account Creation
+Cypress.Commands.add('signUpAndCreateAccount', (user) => {
+  cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+  cy.get('[data-qa="signup-name"]').type(user.name, { delay: 100 });
+  cy.get('[data-qa="signup-email"]').type(user.email, { delay: 100 });
+  cy.get('[data-qa="signup-button"]').click();
+
+  cy.get('#id_gender1').check('Mr');
+  cy.get('[data-qa="password"]').type(user.password, { delay: 100 });
+  cy.get('[data-qa="days"]').select(user.day, { delay: 100 });
+  cy.get('[data-qa="months"]').select(user.month, { delay: 100 });
+  cy.get('[data-qa="years"]').select(user.year, { delay: 100 });
+  cy.get('#newsletter').check();
+  cy.get('#optin').check();
+
+  cy.get('[data-qa="first_name"]').type(user.firstName, { delay: 100 });
+  cy.get('[data-qa="last_name"]').type(user.lastName, { delay: 100 });
+  cy.get('[data-qa="company"]').type(user.company, { delay: 100 });
+  cy.get('[data-qa="address"]').type(user.address, { delay: 100 });
+  cy.get('[data-qa="address2"]').type(user.address2, { delay: 100 });
+  cy.get('[data-qa="country"]').select(user.country, { delay: 100 });
+  cy.get('[data-qa="state"]').type(user.state, { delay: 100 });
+  cy.get('[data-qa="city"]').type(user.city, { delay: 100 });
+  cy.get('[data-qa="zipcode"]').type(user.zipcode, { delay: 100 });
+  cy.get('[data-qa="mobile_number"]').type(user.mobile, { delay: 100 });
+
+  cy.get('[data-qa="create-account"]').click();
+  cy.get('.title > b').should('have.text', 'Account Created!');
+  cy.get('[data-qa="continue-button"]').click();
+});
+
+// 2. User Authentication
+Cypress.Commands.add('login', (email, password, displayName) => {
+  cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+  cy.get('[data-qa="login-email"]').type(email, { delay: 100 });
+  cy.get('[data-qa="login-password"]').type(password, { delay: 100 });
+  cy.get('[data-qa="login-button"]').click();
+  if (displayName) {
+    cy.get('.shop-menu > .nav > :nth-child(10) > a').should('contain.text', `Logged in as ${displayName}`);
+  }
+});
+
+// 3. Product & Cart Flow
+Cypress.Commands.add('addFeaturedProductToCart', (productId, quantity) => {
+  cy.scrollTo('bottom');
+  cy.wait(1000);
+  cy.scrollTo('top');
+  cy.contains('h2', 'Features Items').scrollIntoView();
+
+  cy.get(`a[href="/product_details/${productId}"]`).click();
+  cy.scrollTo('bottom');
+  cy.wait(1000);
+  cy.scrollTo('top');
+
+  cy.get('#quantity').clear().type(quantity);
+  cy.contains('button', 'Add to cart').click();
+  cy.get('.modal-content').should('be.visible');
+  cy.get('.modal-content').contains('Your product has been added to car').should('be.visible');
+  cy.get('.modal-content').contains('Continue Shopping').click();
+
+  cy.contains('button', 'Add to cart').click({ force: true });
+  cy.get('.modal-content a[href="/view_cart"]').click();
+});
+
+// 4. Cart Verification
+Cypress.Commands.add('verifyCartDetails', (itemName, price, quantity) => {
+  cy.get('.cart_info').contains(itemName).should('be.visible');
+  cy.get('.cart_info').contains(price).should('be.visible');
+  cy.get('.cart_info').contains(quantity).should('be.visible');
+
+  cy.get('#cart_info')
+    .should('be.visible')
+    .then(($element) => {
+      const rawText = $element.text();
+      cy.log('The extracted cart text is: ' + rawText);
+      const priceMatch = rawText.match(/Rs\.\s?\d+/);
+      if (priceMatch) {
+        const extractedTotal = priceMatch[0];
+        cy.log('Extracted Total Price is: ' + extractedTotal);
+        expect(extractedTotal).to.contain('Rs.');
+      }
+    });
+
+  cy.get('.cart_info').contains('Total').should('be.visible');
+});
+
+// 5. Checkout & Payment
+Cypress.Commands.add('processCheckoutAndPayment', (paymentDetails) => {
+  cy.get('.btn.btn-default.check_out').click();
+  cy.get('.heading').contains('Address Details').should('be.visible');
+  cy.get('.page-subheading').contains('Your delivery address').should('be.visible');
+  cy.get('.page-subheading').contains('Your billing address').should('be.visible');
+
+  cy.get('a[href="/payment"]').click();
+  cy.get('.heading').contains('Payment').should('be.visible');
+
+  cy.get('[data-qa="name-on-card"]').type(paymentDetails.nameOnCard);
+  cy.get('[data-qa="card-number"]').type(paymentDetails.cardNumber);
+  cy.get('[data-qa="cvc"]').type(paymentDetails.cvc);
+  cy.get('[data-qa="expiry-month"]').type(paymentDetails.expiryMonth);
+  cy.get('[data-qa="expiry-year"]').type(paymentDetails.expiryYear);
+
+  cy.get('[data-qa="pay-button"]').click();
+  cy.get('[data-qa="order-placed"]').contains('Order Placed!').should('be.visible');
+  cy.contains('p', 'Congratulations! Your order has been confirmed!').should('be.visible');
+
+  cy.get('.btn.btn-default.check_out').should('be.visible');
+  cy.get('.btn.btn-primary').should('be.visible');
+  cy.get('.btn.btn-default.check_out').click();
+  cy.get('[data-qa="continue-button"]').click();
+});
+// 6. Verify Empty Cart
+Cypress.Commands.add('verifyCartIsEmpty', () => {
+  cy.get('.shop-menu > .nav > :nth-child(3) > a').click(); 
+  cy.get('#empty_cart').should('be.visible');
+  cy.get('#empty_cart').contains('Cart is empty!').should('be.visible');
+});
